8c2d337ea344c942aba237b19704881c065a126b
Shall16 <shall130@rsdmo.org>
2018-06-26 16:53:52 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -6,5 +6,8 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index #when we get to our homepage go to an embeded ruby file named index
   end
+gets '/iHopeThisWorks' do 
+  erb :page2
+end 
 
 end
